b0c1efa90085a15cdadfb447fee9d7929102cc00
GodOfPro <1387ilia@gmail.com>
2024-03-18 22:44:55 +0330

    Refactor the views package for better extendability

--- internal/webapp/lobby.go
+++ internal/webapp/lobby.go
@@ -2,9 +2,9 @@ package webapp
 
 import (
 	"github.com/labstack/echo/v4"
-	"kingscomp/internal/webapp/views"
+	"kingscomp/internal/webapp/views/lobby"
 )
 
 func (w *WebApp) lobbyIndex(c echo.Context) error {
-	return HTML(c, views.LobbyIndex())
+	return HTML(c, lobby.Index())
 }
